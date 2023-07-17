cbd15212f63e4d0d4777c977ca504964b23552d5
PaoloR96 <134693980+PaoloR96@users.noreply.github.com>
2023-07-17 14:24:28 +0200

    save
    save

--- STM32/workspace/Progetto/Master/Master/Master/Debug/Core/LIBR/Metodi/ManageStr.d
+++
@@ -1,3 +0,0 @@
-Core/LIBR/Metodi/ManageStr.o: ../Core/LIBR/Metodi/ManageStr.c \
- ../Core/LIBR/Metodi/ManageStr.h
-../Core/LIBR/Metodi/ManageStr.h:
--- STM32/workspace/Progetto/Master/Master1.0/Master/Debug/Core/LIBR/fuzzy/fuzzy.d
+++
@@ -1,3 +0,0 @@
-Core/LIBR/fuzzy/fuzzy.o: ../Core/LIBR/fuzzy/fuzzy.c \
- ../Core/LIBR/fuzzy/fuzzy.h
-../Core/LIBR/fuzzy/fuzzy.h:
